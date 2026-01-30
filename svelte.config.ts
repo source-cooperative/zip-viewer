7212ea9c70788d4a5069283d3dcbf4b980be0ee8
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2026-01-29 17:07:27 -0800

    Add pages config

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,4 +1,18 @@
 import adapter from "@sveltejs/adapter-static";
 import type { Config } from "@sveltejs/kit";
 
-export default { kit: { adapter: adapter() } } satisfies Config;
+export default {
+    kit: {
+      adapter: adapter({
+        pages: "docs",
+        assets: "docs",
+        fallback: undefined,
+        precompress: false,
+        strict: true
+      }),
+      paths: {
+        base: "/zip-viewer",
+        relative: false
+      }
+    }
+  } satisfies Config;
